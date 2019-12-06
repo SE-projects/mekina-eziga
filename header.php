8bdae626989b7dee8fcfe0459e0beba4cf8d7479
MickyBe <bmikiyas@gmail.com>
2019-12-06 21:24:16 +0300

    home page with bootstrap

--- header.php
+++ header.php
@@ -3,39 +3,62 @@
 	session_start();
  ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>login system</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-	
-	<header id="header" class="">
-		<nav>
-			<div class="main-wrapper">
-				
-				<ul>
-				
-					<li><a href="index.php" title=""><img src="img/mekina.png"alt="logo">Home</a></li>
-				</ul>
-				<div class="nav-login">
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Complete Bootstrap 4 Website Layout</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+	<link href="style.css" rel="stylesheet">
+ </head>
+ <body>
+
+ <!-- Navigation -->
+ <nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+	<div class="container-fluid">
+		<a href="" class="navbar-brand" href="#"><img src="img/logo1.png" alt=""></a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarResponsive">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="#">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">About</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Services</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Team</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Connect</a>
+				</li>
 					<?php
 						if (isset($_SESSION['u_id'])) {
 							echo '<form action="includes/logout.inc.php" method="POST">
-								<button type="submit" name = "submit">Logout</button>
+								<button type="submit" name = "submit" class="btn btn-primary btn-lg">Logout</button>
 								</form>';
 						} else {
 							echo 	'<form action="includes/login.inc.php" method="POST">
 									<input type="text" name="uid" placeholder="Username/e-mail">
-									<input type="password" name="pwd" placeholder="password">
-									<button type="submit" name="submit">Login</button>
+									<input  type="password" name="pwd" placeholder="password">
+									<button type="submit" name="submit" class="btn btn-primary btn-lg">Login</button>
 									</form>
-									<a href="signup.php">Sing up</a>';
+									<li class="nav-item">
+									<a class="nav-link" href="signup.php">Sing up</a>
+										</li>';
 						}
 					?>
-				</div>
-			</div>
-		</nav>
-	</header><!-- /header -->
+			</ul>
+		</div>
+	</div>
+ </nav>
--- index.php
+++ index.php
@@ -1,23 +1,228 @@
 <?php 
 	include_once 'header.php';
  ?>
+<!--- Image Slider -->
 
-
-
-	<section class="main-container">
-		<div class="main-wrapper">
-			<h2>Home</h2>
-			<?php  
-
-				if (isset($_SESSION['u_id'])) {
-					echo "You are logged in!";
-				}
-
-			?>
-
+<div id="slides" class="carousel slide" data-ride="carousel">
+	<ul class="carousel-indicators">
+		<li data-target="#slides" data-slide-to="0" class="active"></li>
+		<li data-target="#slides" data-slide-to="1"></li>
+		<li data-target="#slides" data-slide-to="2"></li>
+	</ul>
+	<div class="carousel-inner">
+		<div class="carousel-item active">
+			<img src="img/mbackground.jpeg">
+			<div class="carousel-caption">
+				<h1 class="display-2">Mekina-Eziga</h1>
+				<h3>Used cars sales</h3>
+				<button type="button" class="btn btn-outline-light btn-lg">VIEW DEMO</button>
+				<button type="button" class="btn btn-primary btn-lg">Get Started</button>
+			</div>
 		</div>
-	</section>
+		<div class="carousel-item">
+				<img src="img/mbackground2.jpeg" alt="">
+		</div>
+		<div class="carousel-item">
+				<img src="img/mbackground3.jpeg" alt="">
+		</div>
+	</div>
+</div>
+
+<!--- Jumbotron -->
+<div class="container-fluid">
+	<div class="row jumbotron">
+		<div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-10">
+			<p class="lead">The purpose of this site is to automate a used cars sales from the purchasing of the cars from the customers to the sales of the cars to the customers</p>
+		</div>
+		<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-2">
+			<a href="#"><button type="button" class="btn btn-outline-secondary btn-lg">start</button></a>
+		</div>
+	</div>
+</div>
+
+<!--- Welcome Section -->
+<div class="container-fluid padding">
+	<div class="row welcome text-center">
+		<div class="col-12">
+			<h1 class="display-4">Buy and sell with ease.</h1>
+		</div>
+		<hr>
+		<div class="col-12">
+			<p class="lead">Welcome to my Mekina eziga website! Mekina eziga automates a used cars sales from the purchasing of the cars from the customers to the sales of the cars to the customers</p>
+		</div>
+	</div>
+</div>
+
+<!--- Three Column Section -->
+<div class="container-fluid padding">
+	<div class="row text-center padding">
+		<div class="col-xs-12 col-sm-6 col-md-4">
+			<i class="fas fa-car"></i>
+			<h3>Seller</h3>
+			<p>Rigisteer as a seller to sell your cars
+		</p>
+		</div>
+		<div class="col-xs-12 col-sm-6 col-md-4">
+			<i class="fas fa-bold"></i>
+			<h3>Buyer</h3>
+			<p>We have more than 100k rigisterd buyers</p>
+		</div>
+		<div class="col-sm-12 col-sm-6 col-md-4">
+			<i class="fas fa-car"></i>
+			<h3>dealer</h3>
+			<p>There are high quality delers that buy and sell cars for a resenable price </p>
+		</div>
+	</div>
+	<hr class="my-4">
+</div>
+
+<!--- Two Column Section -->
+<div class="container-fluid padding">
+	<div class="row padding">
+		<div class="col-lg-6">
+			<h2>We stive for execelence</h2>
+			<p>Here at  Mekina eziga, Inc. ; We strive to provide the best customer service and satisfaction to our customers.</p> 
+			<p>We price our vehicles at a whole sale price. You will see that we don't price like any other regular dealerships out there. Inventory is received every week, and our website is updated daily.</p>
+			<p> As a dealership with expertise, we like and prefer to deal with clean and reliable vehicles. We do not like to deal with salvaged vehicles; all vehicles have a clean title.</p>
+			<br>
+			<a href="" class="btn btn-primary">Learn More</a>
+		</div>
+		<div class="col-lg-6">
+			<img src="img/desk1.jpeg" class="img-fluid">
+		</div>
+	</div>
+</div>
+
+<hr class="my-4">
+
+
+
+  
+<<!--- Meet the team -->
+<div class="container-fluid padding">
+	<div class="row welcome text-center">
+		<div class="col-12">
+			<h1 class="display">sample of Car for sell</h1>
+		</div>
+		<hr>
+	</div>
+</div>
+
+<!--- Cards -->
+<div class="container-fluid padding">
+	<div class="row padding">
+		<div class="col-md-4">
+			<div class="card">
+				<img class="card-img-top" src="img/car-for-sale3.jpeg">
+				<div class="card-body">
+					<h4 class="card-title">lambo</h4>
+					<p class="card-text">John is an Internet entrepreneur with almost 20 years of experience.</p>
+					<a href="#" class="btn btn-outline-secondary">Make a bid</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="card">
+				<img class="card-img-top" src="img/car-for-sale.jpg">
+				<div class="card-body">
+					<h4 class="card-title">classic cx-r</h4>
+					<p class="card-text">Mary is a designer with almost 10 years of digital design experience.</p>
+					<a href="#" class="btn btn-outline-secondary">Make a bid</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="card">
+				<img class="card-img-top" src="img/car-for-sale2.jpeg">
+				<div class="card-body">
+					<h4 class="card-title">Custome v8</h4>
+					<p class="card-text">Phil is a developer with over 5 years of web development experience.</p>
+					<a href="#" class="btn btn-outline-secondary">Make a bid</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--- Two Column Section -->
+<div class="container-fluid padding">
+		<div class="row padding">
+			<div class="col-lg-6">
+				<h2>Our Philosophy</h2>
+				<p>We know that greatness in a disruptive era requiress bold ambition, curious talent and a culture that believes we're smarter together.</p>
+				<p>We approach every challenge holistically, with best-in-class expertise in data, creativity, media, technology, search, social and more.</p>
+				
+			</div>
+			<div class="col-lg-6">
+				<img src="img/Philosophy.jpeg" class="img-fluid">
+			</div>
+		</div>
+		<hr class="my-4">
+	</div>
+
+<!--- Connect -->
+<div class="container-fluid padding">
+	<div class="row text-center padding">
+		<div class="col-12">
+			<h2>Connect</h2>
+		</div>
+		<div class="col-12 social padding">
+			<a href="#"><i class="fab fa-facebook"></i></a>
+			<a href="#"><i class="fab fa-twitter"></i></a>
+			<a href="#"><i class="fab fa-google-plus-g"></i></a>
+			<a href="#"><i class="fab fa-instagram"></i></a>
+			<a href="#"><i class="fab fa-youtube"></i></a>
+		</div>
+	</div>
+</div>
+
+<!--- Footer -->
+<footer>
+	<div class="container-fluid padding">
+		<div class="row text-center">
+			<div class="col-md-4">
+				<img src="img/mekina.png">
+				<hr class="light">
+				<p>555-555-5555</p>
+				<p>Mekina-eziga@myemail.com</p>
+				<p>100 Street Name</p>
+				<p>Addis Ababa, State, 00000</p>
+			</div>
+			<div class="col-md-4">
+				<hr class="light">
+				<h5>Our hours</h5>
+				<hr class="light">
+				<p>Monday: 9am to 5pm</p>
+				<p>Saturday: 10am to 4pm</p>
+				<p>Sunday: Closed</p>
+			</div>
+			<div class="col-md-4">
+				<hr class="light">
+				<h5>Service Area</h5>
+				<hr class="light">
+				<p>City, State, 00000</p>
+				<p>City, State, 00000</p>
+				<p>City, State, 00000</p>
+				<p>City, State, 00000</p>
+			</div>
+			<div class="col-12">
+				<hr class="light-100">
+				<h5>&copy; MekinaEziga.com</h5>
+			</div>
+		</div>
+	</div>
+</footer>
 
 <?php 
 	include_once 'footer.php';
 ?>
+
+
+
+
+
+
+
+
+
+
+
