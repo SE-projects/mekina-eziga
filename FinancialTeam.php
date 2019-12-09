552c8e423db501b242b826381abf9cfcd3722192
Mulat Yazew <gebremedhnyazewaait@gmail.com>
2019-12-09 06:45:15 +0300

    financial team page

---
+++ FinancialTeam.php
@@ -0,0 +1,48 @@
+<!doctype html>
+
+
+<html>
+
+
+<head>
+ <link rel="stylesheet" type="text/css" href="mekezi.css">
+ <meta charset="UTF-8">
+ <meta http-equiv="author" content="Mulat Yazew">
+</head>
+	
+<body>
+
+<h1>This is financial team page</h1>
+    <!--This is when financial team login into the system and it gives options to
+	give one of the services he/she prefers.-->
+	<p>Please follow the following procedures below to get a service!!!</p>
+	<form id="financialPage" action="" method="">
+	<p>To make a bid click on the next button</p>
+	<input id="b1"type="button" onclick="" value="Make a bid">
+	<p>To cancel a bid click on the next button.</p>
+	<input id="b2"type="button" onclick="" value="Cancel a bid">
+	<p>To see maximum bid click on the next button</p>
+	<input id="b3"type="button" onclick="" value="See maximum bid">
+	<p>To pay commission for mediator click on the next button.</p>
+	<input id="b4"type="button" onclick="" value="Pay commission for mediator">
+	</form>
+	<!--This page is displayed when the financial team clicks one of the buttons above.-->
+	<p>fill the following information either to make a bid or cancel the a bid</p> 
+	<div id="afterclick">
+	<form action="" method="">
+	<label for="amount">Bidding Amount<label>
+	<input type="text"id="amount" name="amount">
+	<label for="model">Car model<label>
+	<input type="text"id="model" name="model">
+	<label for="year">Year of production<label>
+	<input type="text"id="year" name="year">
+	<input type="submit" name="Submit">
+	</form>
+	</div>
+	
+</body>
+
+
+</html>
+
+
