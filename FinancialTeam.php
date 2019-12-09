ea177ff0f5449e6ef2a4ce03fe1824179b3a16ca
Mulat Yazew <gebremedhnyazewaait@gmail.com>
2019-12-09 07:19:05 +0300

    Page after financial team login.

--- FinancialTeam.php
+++ FinancialTeam.php
@@ -1,48 +1,79 @@
-<!doctype html>
+ 
+<?php 
+  include_once 'header.php';
+ ?>
 
+ <div class="container">
 
-<html>
+      <!-- Page Heading -->
+      <h1 class="my-4">Cars to be bidded   
+        <small>Secondary Text</small>
+      </h1>
 
+      <!-- Project One -->
+      <hr>
+      <div class="row">
+        <div class="col-md-7">
+          <a href="#">
+            <img class="img-fluid rounded mb-3 mb-md-0" src="img/car.png/600x300" alt="">
+          </a>
+        </div>
+        <div class="col-md-5">
+          <h3>Description </h3>
+          <p>If you are going to use our system to buy used cars you can click on the <b><i>Make a bid</i></b> and then the system asks you to choose the car model you want to buy and the amount for bid. then you will be notified if you are the winner within an alloted perid of time for bidding.</p>
+          <a class="btn btn-primary" href="#">Make a bid</a>
+        </div>
+      </div>
+  </hr>
+      <!-- /.row -->
 
-<head>
- <link rel="stylesheet" type="text/css" href="mekezi.css">
- <meta charset="UTF-8">
- <meta http-equiv="author" content="Mulat Yazew">
-</head>
-	
-<body>
+      <hr>
 
-<h1>This is financial team page</h1>
-    <!--This is when financial team login into the system and it gives options to
-	give one of the services he/she prefers.-->
-	<p>Please follow the following procedures below to get a service!!!</p>
-	<form id="financialPage" action="" method="">
-	<p>To make a bid click on the next button</p>
-	<input id="b1"type="button" onclick="" value="Make a bid">
-	<p>To cancel a bid click on the next button.</p>
-	<input id="b2"type="button" onclick="" value="Cancel a bid">
-	<p>To see maximum bid click on the next button</p>
-	<input id="b3"type="button" onclick="" value="See maximum bid">
-	<p>To pay commission for mediator click on the next button.</p>
-	<input id="b4"type="button" onclick="" value="Pay commission for mediator">
-	</form>
-	<!--This page is displayed when the financial team clicks one of the buttons above.-->
-	<p>fill the following information either to make a bid or cancel the a bid</p> 
-	<div id="afterclick">
-	<form action="" method="">
-	<label for="amount">Bidding Amount<label>
-	<input type="text"id="amount" name="amount">
-	<label for="model">Car model<label>
-	<input type="text"id="model" name="model">
-	<label for="year">Year of production<label>
-	<input type="text"id="year" name="year">
-	<input type="submit" name="Submit">
-	</form>
-	</div>
-	
-</body>
+      <!-- Project Two -->
+      <div class="row">
+        <div class="col-md-7">
+          <a href="#">
+            <img class="img-fluid rounded mb-3 mb-md-0" src="img/car.png/600x300" alt="">
+          </a>
+        </div>
+        <div class="col-md-5">
+          <h3>Project Two</h3>
+          <p>If want to cancel or update the previous bid you can click on the <b><i>Cancel a bid</i></b> button and the system asks you to choose the car model you want to cancel and then you can update the bidding if the expiration date is no reached.</p>
+          <a class="btn btn-primary" href="#">Cancel a bid</a>
+        </div>
+      </div>
 
+      <hr>
 
-</html>
-
-
+      <!-- Pagination -->
+      <hr>
+      <ul class="pagination justify-content-center">
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+            <span class="sr-only">Previous</span>
+          </a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">1</a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">2</a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">3</a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+            <span class="sr-only">Next</span>
+          </a>
+        </li>
+      </ul>
+</hr>
+    </div>
+</hr>
+    <?php 
+  include_once 'footer.php';
+?>
+    <!-- /.container -->
