ded01d454e970243367404c01ddfb281860352a5
MickyBe <bmikiyas@gmail.com>
2019-12-19 23:03:00 +0300

    seting the signe up page for dealer seler and buyer registeration

--- signup.php
+++ signup.php
@@ -1,88 +1,248 @@
-
 <?php 
-  include_once 'header.php';
+	include_once 'header.php';
  ?>
 
 
 <style>
-.container {
-  position: relative;
-  width: 100%;
-  max-width: 400px;
+.register{
+    background: -webkit-linear-gradient(left, #3931af, #00c6ff);
+    margin-top: 3%;
+    padding: 3%;
 }
-
-.container img {
-  width: 100%;
-  height: auto;
+.register-left{
+    text-align: center;
+    color: #fff;
+    margin-top: 4%;
 }
-
-.container .btn {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  -ms-transform: translate(-50%, -50%);
-  background-color: #555;
-  color: white;
-  font-size: 16px;
-  padding: 12px 24px;
-  border: none;
-  cursor: pointer;
-  border-radius: 5px;
-  text-align: center;
+.register-left input{
+    border: none;
+    border-radius: 1.5rem;
+    padding: 2%;
+    width: 60%;
+    background: #f8f9fa;
+    font-weight: bold;
+    color: #383d41;
+    margin-top: 30%;
+    margin-bottom: 3%;
+    cursor: pointer;
 }
-
-.container .btn:hover {
-  background-color: black;
+.register-right{
+    background: #f8f9fa;
+    border-top-left-radius: 10% 50%;
+    border-bottom-left-radius: 10% 50%;
+}
+.register-left img{
+    margin-top: 15%;
+    margin-bottom: 5%;
+    width: 25%;
+    -webkit-animation: mover 2s infinite  alternate;
+    animation: mover 1s infinite  alternate;
+}
+@-webkit-keyframes mover {
+    0% { transform: translateY(0); }
+    100% { transform: translateY(-20px); }
+}
+@keyframes mover {
+    0% { transform: translateY(0); }
+    100% { transform: translateY(-20px); }
+}
+.register-left p{
+    font-weight: lighter;
+    padding: 12%;
+    margin-top: -9%;
+}
+.register .register-form{
+    padding: 10%;
+    margin-top: 10%;
+}
+.btnRegister{
+    float: right;
+    margin-top: 10%;
+    border: none;
+    border-radius: 1.5rem;
+    padding: 2%;
+    background: #0062cc;
+    color: #fff;
+    font-weight: 600;
+    width: 50%;
+    cursor: pointer;
+}
+.register .nav-tabs{
+    margin-top: 3%;
+    border: none;
+    background: #0062cc;
+    border-radius: 1.5rem;
+    width: 28%;
+    float: right;
+}
+.register .nav-tabs .nav-link{
+    padding: 2%;
+    height: 34px;
+    font-weight: 600;
+    color: #fff;
+    border-top-right-radius: 1.5rem;
+    border-bottom-right-radius: 1.5rem;
+}
+.register .nav-tabs .nav-link:hover{
+    border: none;
+}
+.register .nav-tabs .nav-link.active{
+    width: 100px;
+    color: #0062cc;
+    border: 2px solid #0062cc;
+    border-top-left-radius: 1.5rem;
+    border-bottom-left-radius: 1.5rem;
+}
+.register-heading{
+    text-align: center;
+    margin-top: 8%;
+    margin-bottom: -15%;
+    color: #495057;
+}
 }
 </style>
-  <section class="main-container">
-    <div class="main-wrapper">
-      <h2>Signup</h2>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+<section class="main-container">
+	<div class="main-wrapper">
+			<h2>Signup</h2>
+		<form class="signup-form" action="includes/signup.inc.php" method="POST">
+			<div class="container register">
+                <div class="row">
+                    <div class="col-md-3 register-left">
+                        <img src="https://image.ibb.co/n7oTvU/logo_white.png" alt=""/>
+                        <h3>Welcome</h3>
+                        <p>You are 30 seconds away from earning your own money!</p>
+                        <input type="submit" name="" value="Login"/><br/>
+                    </div>
+                    <div class="col-md-9 register-right">
+                        <ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">USERE</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">DEALER</a>
+                            </li>
+                        	</ul>
+                        <div class="tab-content" id="myTabContent">
+                            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                                <h3 class="register-heading">Apply as a User</h3>
+                                <div class="row register-form">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <input type="text" name="first" class="form-control" placeholder="First Name " value="" />
+                                         
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="text" name="last" class="form-control" placeholder="Last Name *" value="" />
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" name="pwd" class="form-control" placeholder="Password *" value="" />
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" name="confpwd" class="form-control"  placeholder="Confirm Password *" value="" />
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="maxl">
+                                            <p>What are you</p>
+                                                <label class="radio inline"> 
+                                                    <input type="radio" name="gender" value="seller" checked>
+                                                    <span> SELLER </span> 
+                                                </label>
+                                                <label class="radio inline"> 
+                                                    <input type="radio" name="gender" value="buyer">
+                                                    <span>BUYER</span> 
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <input type="email" name="email"   class="form-control" placeholder="Your Email *" value="" />
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="text"  minlength="10" maxlength="10" name="txtEmpPhone" class="form-control" placeholder="Your Phone *" value="" />
+                                        </div>
+                                        <div class="form-group">
+                                            <select class="form-control">
+                                                <option class="hidden"  selected disabled>Please select your Sequrity Question</option>
+                                                <option>What is your Birthdate?</option>
+                                                <option>What is Your old Phone Number</option>
+                                                <option>What is your Pet Name?</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" placeholder="Enter Your Answer *" value="" />
+                                        </div>
+                                        <input type="submit" name="submit-user" class="btnRegister"  value="Register"/>
+                                    </div>
+                                </div>
+                            </div>
+                    <div class="tab-pane fade show" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                                <h3  class="register-heading">Apply as a Hirer</h3>
+                                <div class="row register-form">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <input type="text" name="first" class="form-control" placeholder="First Name" value="" />
+                                      
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="text" name="last" class="form-control" placeholder="Last Name" value="" />
+                                            
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="email" name="email" class="form-control" placeholder="Email " value="" />
+                                            
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="text" maxlength="10" minlength="10" class="form-control" placeholder="Phone *" value="" />
+                                        </div>
 
-      <div class="container-fluid padding">
-        <div class="row text-center padding">
-        
-        <div class="col-xs-12 col-sm-6 col-md-4">
-        <div class="container">
-           <img src="img/mbackground.jpeg" alt="Snow" style="width:100%">
-          <button class="btn" onclick="window.location.href = 'SellerSignUp.php';">SELLER</button> 
-          </div>
-        </div>
+                                        	<div class="form-group">
+                                            <input type="text" name="uid" maxlength="10" minlength="10" class="form-control" placeholder="Username" value="" />
+                                           
+                                        </div>
+				
+			
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <input type="password" name="pwd" class="form-control" placeholder="Password *" value="" />
 
-        <div class="col-xs-12 col-sm-6 col-md-4">
-        <div class="container">
-           <img src="img/mbackground.jpeg" alt="Snow" style="width:100%">
-          <button class="btn" onclick="window.location.href = 'BuyerSignUp.php';">BUYER</button> 
-          </div>
-        </div>
+		
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" name="pwd-repeat" class="form-control" placeholder="Confirm Password *" value="" />
+                                        </div>
+                                        <div class="form-group">
+                                            <select class="form-control">
+                                                <option class="hidden"  selected disabled>Please select your Sequrity Question</option>
+                                                <option>What is your Birthdate?</option>
+                                                <option>What is Your old Phone Number</option>
+                                                <option>What is your Pet Name?</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" placeholder="`Answer *" value="" />
+                                        </div>
+                                        <input type="submit" name="submit-dealer" class="btnRegister"  value="Register"/>
+                                    </div>
+                                </div>
+                            </div>
+                        
+                        </div>
+                    </div>
+                </div>
 
-        <div class="col-sm-12 col-sm-6 col-md-4">
-        <div class="container">
-           <img src="img/mbackground.jpeg" alt="Snow" style="width:100%">
-          <button class="btn" onclick="window.location.href = 'DealerSignUp.php';">DEALER</button> 
-          
+            </div>
+			</div>
+			</form>
+		</div>
+	</section>
 
-          </div>
-        </div>
-        </div>
-        <hr class="my-4">
-      </div>
-     
-
-    <!-- <form class="signup-form" action="includes/signup.inc.php" method="POST">
-        <input type="text" name="first" placeholder="Firstname">
-        <input type="text" name="last" placeholder="Lastname">
-        <input type="text" name="email" placeholder="E-mail">
-        <input type="text" name="uid" placeholder="Username">
-        <input type="password" name="pwd" placeholder="Password">
-        <input type="text" name="pnum" placeholder="Phone Number">
-    
-        <button type="submit" name="submit">Sign up</button>
-      </form> -->
-    </div>
-  </section>
 
 <?php 
-  include_once 'footer.php';
+	include_once 'footer.php';
 ?>
