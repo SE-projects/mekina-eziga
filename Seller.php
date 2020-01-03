184307222205f6b354cb577b0f2432b1ac7566bd
HeniT11 <56683081+HeniT11@users.noreply.github.com>
2020-01-03 12:18:42 +0300

    Last Commit

--- Seller.php
+++ Seller.php
@@ -1,104 +1,228 @@
-<?php
-	require 'header.php';
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
 
-?> 
-<main>
-<!-- Page Content -->
-<div class="container">
+<head>
 
-  <!-- Portfolio Item Heading -->
-  <h1 class="my-4">Post car
-    <small>insert all nesesary information</small>
-  </h1>
+    <meta charset="utf-8">
+    <meta name="robots" content="all,follow">
+    <meta name="googlebot" content="index,follow,snippet,archive">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Obaju e-commerce template">
+    <meta name="author" content="Ondrej Svestka | ondrejsvestka.cz">
+    <meta name="keywords" content="">
 
-  <!-- Portfolio Item Row -->
-  <div class="row">
+    <title>
+        Mekina Eziga
+    </title>
 
-    <div class="col-md-8">
-      <img class="img-fluid" src="http://placehold.it/750x500" alt="">
-      <button type="submit" name = "submit" class="btn btn-secondary btn-lg">chosse car picture</button>
-      
-    </div>
+    <meta name="keywords" content="">
 
-    <div class="col-md-4">
-      <h3 class="my-3">Car Description</h3>
-      <div class="form-group">
-  <label for="comment">delail car description:</label>
-  <textarea class="form-control" rows="5" id="comment"></textarea>
-</div>
-      <h3 class="my-3">Car Details</h3>
-      <ul>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,500,700,300,100' rel='stylesheet' type='text/css'>
+
+    <!-- styles -->
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/animate.min.css" rel="stylesheet">
+    <link href="css/owl.carousel.css" rel="stylesheet">
+    <link href="css/owl.theme.css" rel="stylesheet">
+
+    <!-- theme stylesheet -->
+    <link href="css/style.default.css" rel="stylesheet" id="theme-stylesheet">
+
+    <!-- your stylesheet with modifications -->
+    <link href="css/custom.css" rel="stylesheet">
+
+    <script src="js/respond.min.js"></script>
+
+    <link rel="shortcut icon" href="favicon.png">
+
+
+
+</head>
+
+<body>
+
+  <?php $db = mysqli_connect('localhost','root','','shop')
+    or die('Error connecting to MySQL server.'); 
+
+    $query1 = "SELECT * FROM category ";
+    $result1 = mysqli_query($db, $query1);
+    $categories = mysqli_fetch_array($result1);
+
+  ?>
+  <?php include 'header.php';?>
+
+            <!--/.nav-collapse -->
+
+    <div id="all">
+
+        <div id="content">
+
+           
+
+            <!-- *** ADVANTAGES HOMEPAGE ***
+ _________________________________________________________ -->
+            <div id="advantages">
+
+                <div class="container">
+                    <div class="same-height-row">
+                        <div class="col-sm-4">
+                            <div class="box same-height clickable">
+                                <div class="icon"><i class="fa fa-heart"></i>
+                                </div>
+
+                                <h3><a href="#">We love our customers</a></h3>
+                               
+                            </div>
+                        </div>
+
+                        <div class="col-sm-4">
+                            <div class="box same-height clickable">
+                                <div class="icon"><i class="fa fa-tags"></i>
+                                </div>
+
+                                <h3><a href="#">Best prices</a></h3>
+                               
+                            </div>
+                        </div>
+
+                        <div class="col-sm-4">
+                            <div class="box same-height clickable">
+                                <div class="icon"><i class="fa fa-thumbs-up"></i>
+                                </div>
+
+                                <h3><a href="#">100% satisfaction guaranteed</a></h3>
+                                
+                            </div>
+                        </div>
+                      </div>
+                    <!-- /.row -->
+
+                </div>
+                <!-- /.container -->
+
+            </div>
+            <!-- /#advantages -->
+
+
+
+            <?php 
+            
         
-      	<div class="form-group">
-  <label for="usr">Car modell:</label>
-  <input type="text" class="form-control" id="usr">
-</div>
-<div class="form-group">
-  <label for="pwd">Car mileage:</label>
- <input type="text" class="form-control" id="usr">
- <label for="pwd">Car year of Production:</label>
- <input type="text" class="form-control" id="usr">
+        $f=$_SESSION["userid"];
+        $query = "SELECT * FROM item where sellerID = '$f' order by ItemID DESC";
+        $result = mysqli_query($db, $query);
+        $row = mysqli_fetch_array($result);
+ 
+      ?>
 
-  <label for="pwd">Color:</label>
- <input type="text" class="form-control" id="usr">
+            <div id="hot">
 
-  <label for="pwd">Top Speed:</label>
- <input type="text" class="form-control" id="usr">
+                <div class="box">
+                    <div class="container">
+                        <div class="col-md-12">
 
-  <label for="pwd">Bid Staring Amount:</label>
- <input type="text" class="form-control" id="usr">
+                            <h2>CARS POSTED FOR SELL</h2>
+                        </div>
+                    </div>
+                </div>
 
-  
+                <div class="container">
+                    <div class="product-slider">
+                        
 
-</div>
+                <?php
+                 $count=1;
+        while($row && $count <=10) { 
+        
+          ?>
 
-      </ul>
-    </div>
+         <div class="item">
+                            <div class="product">
+                                <div class="flip-container">
+                                    <div class="flipper">
+                                        <div class="front">
+                                            <a href="managelisting.php?ItemNo=<?php echo $row['ItemID'] ?>">
+                                                <img src="<?php echo $row['PhotosID'];?>" alt="" class="img-responsive">
+                                            </a>
+                                        </div>
+                                        <div class="back">
+                                            <a href="managelisting.php?ItemNo=<?php echo $row['ItemID'] ?>">
+                                                <img src="<?php echo $row['PhotosID'];?>" alt="" class="img-responsive">
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href="managelisting.php?ItemNo=<?php echo $row['ItemID'] ?>" class="invisible">
+                                    <img src="img/product1.jpg" alt="" class="img-responsive">
+                                </a>
+                                <div class="text">
+                                    <h3><a href="managelisting.php?ItemNo=<?php echo $row['ItemID'] ?>"><?php echo $row['ItemName']?></a></h3>
+                                    <p class="price">BIRR : <?php echo number_format($row['CurrentPrice'],2)?></p>
+                  
 
-  </div>
-  <!-- /.row -->
-<button type="submit" name = "submit" class="btn btn-primary btn-lg">Post car for sell</button>
-  <!-- Related Projects Row -->
-  <h3 class="my-4">previouse cars sold</h3>
+                                </div>
+                                <!-- /.text -->
+                            </div>
+                            <!-- /.product -->
+                        </div>
 
-  <div class="row">
+                       <?php $row = $result->fetch_assoc();
+                        $count=$count+1;
+                      }
+          ?>
 
-    <div class="col-md-3 col-sm-6 mb-4">
-      <a href="#">
-            <img class="img-fluid" src="img/car1.jpg" alt="">
-            <p class="form-control">SOLD for : 400,000</p>
-          </a>
-    </div>
+          </div>
+                </div>
 
-    <div class="col-md-3 col-sm-6 mb-4">
-      <a href="#">
-            <img class="img-fluid" src="img/car2.jpg" alt="">
-            <p class="form-control">SOLD for : 350,000</p>
-          </a>
-    </div>
+                
+                <!-- /.container -->
+      </div>
 
-    <div class="col-md-3 col-sm-6 mb-4">
-      <a href="#">
-            <img class="img-fluid" src="img/car3.jpg" alt="">
-            <p class="form-control">SOLD for : 450,000</p>
-          </a>
-    </div>
+        
+            <!-- *** GET INSPIRED ***
+ _________________________________________________________ -->
+           
 
-    <div class="col-md-3 col-sm-6 mb-4">
-      <a href="#">
-            <img class="img-fluid" src="img/car4.jpg" alt="">
-            <p class="form-control">SOLD for : 300,000</p>
-          </a>
-    </div>
+            <!-- *** BLOG HOMEPAGE ***
+ _________________________________________________________ -->
 
-  </div>
-  <!-- /.row -->
+            <div class="box text-center" data-animate="fadeInUp">
+                <div class="container">
+                    <div class="col-md-12">
+                        <h3 class="text-uppercase">Grab products for lowest prices</h3>
 
-</div>
-<!-- /.container -->
+                        <p class="lead">Start Bidding Keep on Winning 
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+           
+
+            <!-- *** BLOG HOMEPAGE END *** -->
 
 
-</main>
-<?php
-   require 'footer.php';
-   ?>
+        </div>
+        <!-- /#content -->
+
+       <?php include 'footer.php';?>
+    
+
+    <!-- *** SCRIPTS TO INCLUDE ***
+ _________________________________________________________ -->
+    <script src="js/jquery-1.11.0.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.cookie.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/modernizr.js"></script>
+    <script src="js/bootstrap-hover-dropdown.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/front.js"></script>
+
+
+
+</body>
+
+</html>
